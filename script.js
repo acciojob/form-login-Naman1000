707d3d6a549a13faaa7a9441101a625e0f8c240e
Naman1000 <91007344+Naman1000@users.noreply.github.com>
2025-03-10 16:36:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ function getFormvalue() {
     //Write your code here
 		  // Prevents the form from reloading the page
 
-            let firstName = document.getElementByName("fname").value.trim();
-            let lastName = document.getElementByName("lname").value.trim();
+            let firstName = document.querySelector("fname").value.trim();
+            let lastName = document.querySelector("lname").value.trim();
 
             if (!firstName || !lastName) {
                 alert("Please enter both First Name and Last Name.");
